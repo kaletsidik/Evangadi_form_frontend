5631041a610b5093dc0af1c2b98b9f521f0b510e
Awoke <awokeworket@gmail.com>
2025-02-28 23:05:27 +0300

    frontend

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const instance = axios.create({
-  // baseURL: "http://localhost:5000/api",
-  baseURL: "evangadi-form-backend-cfck.vercel.app",
+  //baseURL: "http://localhost:5000/api",
+  // baseURL: "evangadi-form-backend-cfck.vercel.app",
+  baseURL: "https://evangadi-form-backend-v500.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
