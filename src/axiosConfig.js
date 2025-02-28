8809b18a85f284cc418ee7c79003801550ed996c
Awoke <awokeworket@gmail.com>
2025-02-28 21:59:03 +0300

    evangadi front end form

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  // baseURL: "http://localhost:5000/api",
+  baseURL: "evangadi-form-backend-cfck.vercel.app",
   headers: {
     "Content-Type": "application/json",
   },
